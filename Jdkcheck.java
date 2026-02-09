ea6f4d3466f957d6c9e35449a5c7417f009cce7b
DamaSaketh <saketh@kernsoftwaresolutions.com>
2026-02-09 13:10:48 +0530

    Delete Jdkcheck.java

--- Jdkcheck.java
+++
@@ -1,6 +0,0 @@
-class Jdkcheck{
-    public static void main(String[] args) {
-        System.out.println("program compile successfully because JDK installed ");
-        System.out.println("JRE is uesd only to run this program");
-    }
-}
